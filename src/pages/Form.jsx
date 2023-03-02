56e4abb8d866be8cc585c63c27cbaa8e9a83201d
RashidaAvshar <rashidaavshar@gmail.com>
2023-03-02 10:45:14 +0400

    create-form-box

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
 const Form = () => {
   return (
-    <div>
-      Form
+    <div  className='container'>
+      <div className="form-box">
+        <div className="form">
+            <h1>REGISTER</h1>
+            <TextField id="standard-basic" label="Name" variant="standard"/>
+            <TextField id="standard-basic" label="Surname" variant="standard" />
+            <TextField id="standard-basic" label="Password" variant="standard" type="password" />
+            <Button variant="contained" color="success">register</Button>
+
+
+        </div>
+      </div>
     </div>
   )
 }
